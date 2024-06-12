058a7bb2cc26bb03a4a47ca61c32d42ab40b6c43
ellaloviv <00001079605149sp@al.educacao.sp.gov.br>
2024-06-12 09:52:49 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,26 @@
+let cor
+let posicaoHorizontal // x
+let posicaoVertical // y 
+function setup() {
+createCanvas(400, 400);
+  background('rgb(255,0,132)');
+  cor = color(random(0,225), random(0,225),random(0,225));
+  posicaoHorizontal = [0,0,0];
+  posicaoVertical = [random(height),random(height),random(height)]
+}
+function draw() {
+  fill(cor) ;
+  for(let contador in posicaoHorizontal) {
+  circle(posicaoHorizontal[contador], posicaoVertical[contador], 50);
+  posicaoHorizontal[contador]+= random(0,3);
+  posicaoVertical[contador]+= random(-3,3);
+    if(posicaoHorizontal[contador] >= width){
+      posicaoHorizontal[contador] = 0;
+      posicaoVertical[contador] = random(height);
+    }
+}
+
+if(mouseIsPressed){
+  cor = (random(0, 225), random(0, 225), random(0, 225))
+}
+}
